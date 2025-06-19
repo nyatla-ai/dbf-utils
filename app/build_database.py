30a5d3a0e255b5f182b9e575cb890618ce8293d6
nyatla-ai <nyatla39+ai@gmail.com>
2025-06-19 22:22:40 +0900

    Update build_database.py

--- app/build_database.py
+++ app/build_database.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 
 from estat_shp_utils.csv_to_sqlite import CsvToSqliteConverter
 
